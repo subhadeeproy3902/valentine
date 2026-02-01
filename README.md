4bc74d89a2df5303a3fadd3e638e75c053a46bc9
subhadeeproy3902 <subha9.5roy350@gmail.com>
2026-02-01 15:47:06 +0530

    first commit

---
+++ README.md
@@ -0,0 +1,36 @@
+# 🥰 Be My Valentine? 🥰
+
+![Project Image](https://i.postimg.cc/WbPD2nwH/image.png)
+
+Welcome to the most adorable project you've ever seen! This is a special React application designed to ask that special someone: "Will you be my Valentine?".
+
+## 💖 What's Inside? 💖
+[Link to site](https://for-valentine.netlify.app)
+
+This project is built with love using React and Vite. It features a cute and interactive UI that playfully nudges the user towards saying "Yes" to being your Valentine.
+
+## 🎁 Features 🎁
+
+- Cute and playful animations
+- Interactive buttons that respond to user interaction
+- Lovely images and heartwarming messages
+
+## 🚀 Getting Started 🚀
+
+To get started with this project, follow these steps:
+
+1. Clone the repository
+2. Install the dependencies with `npm/pnpm/bun install`
+3. Start the development server with `npm/pnpm/bun run dev`
+
+Now you're ready to ask the big question!
+
+## 💕 Made with Love 💕
+
+This project was made with love by [Subhadeep](https://github.com/subhadeeproy3902). Feel free to use it to ask your own special someone to be your Valentine!
+
+## 💌 Feedback and Contributions 💌
+
+Feedback and contributions are always welcome! Feel free to open an issue or submit a pull request.
+
+Happy Valentine's Day! 💘
---
+++ src/App.jsx
@@ -0,0 +1,91 @@
+import { useState } from "react";
+
+export default function Page() {
+  const [noCount, setNoCount] = useState(0);
+  const [yesPressed, setYesPressed] = useState(false);
+  const yesButtonSize = noCount * 20 + 16;
+
+  const handleNoClick = () => {
+    setNoCount(noCount + 1);
+  };
+
+  const getNoButtonText = () => {
+    const phrases = [
+      "No",
+      "Are you sure?",
+      "Really sure?",
+      "Think again!",
+      "Last chance!",
+      "Surely not?",
+      "You might regret this!",
+      "Give it another thought!",
+      "Are you absolutely certain?",
+      "This could be a mistake!",
+      "Have a heart!",
+      "Don't be so cold!",
+      "Change of heart?",
+      "Wouldn't you reconsider?",
+      "Is that your final answer?",
+      "You're breaking my heart ;(",
+      "Is that your final answer?",
+      "You're breaking my heart ;(",
+      "Plsss? :( You're breaking my heart",
+    ];
+
+    return phrases[Math.min(noCount, phrases.length - 1)];
+  };
+
+  return (
+    <div className="overflow-hidden flex flex-col items-center justify-center pt-4 h-screen selection:bg-rose-600 selection:text-white text-zinc-900">
+      {yesPressed ? (
+        <>
+          <img src="https://media.tenor.com/gUiu1zyxfzYAAAAi/bear-kiss-bear-kisses.gif" />
+          <div className="text-4xl md:text-6xl font-bold my-4">
+            Ok Yayyyyy!!!
+          </div>
+        </>
+      ) : (
+        <>
+          <img
+            className="h-[230px] rounded-lg"
+            src="https://media.tenor.com/2csFlkJ3TyAAAAAi/tkthao219-bubududu.gif"
+          />
+          <h1 className="text-4xl md:text-6xl my-4 text-center">
+            Will you be my Valentine?
+          </h1>
+          <div className="flex flex-wrap justify-center gap-2 items-center">
+            <button
+              className={`bg-green-500 hover:bg-green-600 text-white font-bold py-2 px-4 rounded-lg mr-4`}
+              style={{ fontSize: yesButtonSize }}
+              onClick={() => setYesPressed(true)}
+            >
+              Yes
+            </button>
+            <button
+              onClick={handleNoClick}
+              className=" bg-rose-500 hover:bg-rose-600 rounded-lg text-white font-bold py-2 px-4"
+            >
+              {noCount === 0 ? "No" : getNoButtonText()}
+            </button>
+          </div>
+        </>
+      )}
+      <Footer />
+    </div>
+  );
+}
+
+const Footer = () => {
+  return (
+    <a
+      className="fixed bottom-2 left-2 backdrop-blur-md opacity-80 hover:opacity-95 border p-1 rounded border-rose-300"
+      href="https://github.com/subhadeeproy3902/valentine"
+      target="__blank"
+    >
+      Made with{" "}
+      <span role="img" aria-label="heart">
+        ❤️
+      </span>
+    </a>
+  );
+};
